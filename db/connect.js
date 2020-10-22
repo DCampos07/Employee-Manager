bbe193fe097a7e0845fe20449963b2100db81a0a
Damaris C <DCampos07@users.noreply.github.com>
2020-10-22 17:49:05 -0400

    added folders

---
+++ db/connect.js
@@ -0,0 +1,20 @@
+  
+const mysql = require("mysql");
+require('dotenv').config();
+
+
+const connection = mysql.createConnection({
+    host: "localhost",
+
+    // Your port; if not 3306
+    port: 3306,
+
+    // Your username
+    user: "root",
+
+    // Your password
+    password: `${process.env.PASSWORD}`,
+    database: "employee_trackerDB"
+});
+
+module.exports = connection;
